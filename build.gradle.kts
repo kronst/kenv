5ddac2c0110755fb5993fb0c2adacf5b2092e36a
Roman Konstantynovskyi <kronstj@gmail.com>
2024-11-28 00:46:25 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "2.0.21"
+}
+
+group = "dev.kronst"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(11)
+}
